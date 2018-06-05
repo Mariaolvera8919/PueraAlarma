7195cb0793f6497cbf218e43704c0e46c259a729
Mariaolvera8919 <32545926+Mariaolvera8919@users.noreply.github.com>
2018-06-04 21:29:57 -0500

    Update TareaPuerta_Mar15.ino

--- TareaPuerta_Mar15.ino
+++ TareaPuerta_Mar15.ino
@@ -19,7 +19,7 @@ void loop() {
 
 //ciclo donde va a estar parpadeando en 15 iteraciones
  for (int i= 0; i<15;i ++){
-      set_sleep_mode(SLEEP_MODE_PWER_DOEN);//entra en modo sleep
+      set_sleep_mode(SLEEP_MODE_PWR_DOWN);//entra en modo sleep
       digitalWrite(pinLED, LOW);  //se apaga durante 50 milisegundos           
       delay(50);                          
       digitalWrite(pinLED, HIGH); //se enciende durante 50 milisegundos            
