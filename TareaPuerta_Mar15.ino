8b39295bb3e52792dd7ec4e15de9a7be40759072
Mariaolvera8919 <32545926+Mariaolvera8919@users.noreply.github.com>
2018-06-04 21:27:35 -0500

    Update TareaPuerta_Mar15.ino

--- TareaPuerta_Mar15.ino
+++ TareaPuerta_Mar15.ino
@@ -1,4 +1,4 @@
-#include "LowPower.h"
+#include <avr/sleep.h>
 
 
 const int pinSensor = 2;
@@ -13,12 +13,13 @@ void setup() {
  
 void loop() {
 
-//ciclo if donce la alarma empieza como apagada
+ //ciclo if donce la alarma empieza como apagada
  if (digitalRead(pinSensor)== LOW) {
     encender = true;;
 
 //ciclo donde va a estar parpadeando en 15 iteraciones
  for (int i= 0; i<15;i ++){
+      set_sleep_mode(SLEEP_MODE_PWER_DOEN);//entra en modo sleep
       digitalWrite(pinLED, LOW);  //se apaga durante 50 milisegundos           
       delay(50);                          
       digitalWrite(pinLED, HIGH); //se enciende durante 50 milisegundos            
